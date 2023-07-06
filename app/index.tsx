806472a41bf65db3a04d5e56d235b1201201ce4a
seyaobey-dev <seyaobey.dev@gmail.com>
2023-07-06 13:44:23 +0100

    init

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+import { Home } from "../components/home";
+
+export default function Index(props: any) {
+    return (
+        <Home />
+    )
+}
---
+++ components/home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const Home = () => {
+    return (
+        <View style={{ paddingTop: 80, paddingHorizontal: 20 }}>
+            <Text style={{ fontSize: 20 }} >root page</Text>
+        </View>
+    )
+}
